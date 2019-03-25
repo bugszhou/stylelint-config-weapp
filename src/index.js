e7607d78fc5589ec0eecc730abe31952e877d91c
bugszhou <bugszhou@outlook.com>
2019-03-25 22:11:37 +0800

    feat(all): init

---
+++ src/index.js
@@ -0,0 +1,55 @@
+
+module.exports = {
+  "rules": {
+    "selector-type-no-unknown": [
+      true,
+      {
+        "ignoreTypes": [
+          "page",
+          "view",
+          "scroll-view",
+          "swiper",
+          "swiper-item",
+          "movable-view",
+          "movable-area",
+          "cover-view",
+          "cover-image",
+          "icon",
+          "text",
+          "rich-text",
+          "progress",
+          "button",
+          "checkbox-group",
+          "checkbox",
+          "form",
+          "input",
+          "label",
+          "picker",
+          "picker-view",
+          "picker-view-column",
+          "radio-group",
+          "radio",
+          "slider",
+          "switch",
+          "textarea",
+          "navigator",
+          "functional-page-navigator",
+          "audio",
+          "image",
+          "video",
+          "camera",
+          "live-player",
+          "live-pusher",
+          "map",
+          "canvas",
+          "open-data",
+          "web-view",
+          "ad",
+          "official-account",
+          "swiper",
+          "image"
+        ]
+      }
+    ]
+  }
+};
